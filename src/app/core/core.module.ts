23986585bf8916bc5e503293ef92f7f32e33f336
noumanghani <noumansiddique1@gmail.com>
2018-08-20 22:57:21 +0500

    Project is finished

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,22 @@
+import {NgModule} from "@angular/core";
+import {TopNavbarComponent} from "./components/top-navbar/top-navbar.component";
+import {HomeComponent} from "./components/home/home.component";
+import {LoginComponent} from "./components/login/login.component";
+import {RouterModule} from "@angular/router";
+import {SharedModule} from "shared/shared.module";
+
+@NgModule({
+  imports: [
+    SharedModule,
+    RouterModule.forChild([])
+  ],
+  declarations: [
+    TopNavbarComponent,
+    HomeComponent,
+    LoginComponent,
+  ],
+  exports: [
+    TopNavbarComponent,
+  ]
+})
+export class CoreModule { }
--- src/app/shared/services/config.service.ts
+++ src/app/shared/services/config.service.ts
@@ -1,40 +1,40 @@
 import { Injectable } from '@angular/core';
-import {Http, RequestOptions,Headers} from "@angular/http";
+// import {Http, RequestOptions,Headers} from "@angular/http";
 
 @Injectable()
 export class ConfigService {
-  public ip = "http://hopenotout.pk/burger/index.php";
-  public port = '';
-  public api = '';
-  public apiAccount = '';
-  public apiAdmin = '';
-  public apiE2E = '';
-  public masterApi = '';
-  public dateFormat;
-  public token='BuRgRcARt-client-Ang';
-  public token2='ItIsfoRSaFEtY;!@)(@';
-  constructor(private _http:Http) { }
+  // public ip = "http://hopenotout.pk/burger/index.php";
+  // public port = '';
+  // public api = '';
+  // public apiAccount = '';
+  // public apiAdmin = '';
+  // public apiE2E = '';
+  // public masterApi = '';
+  // public dateFormat;
+  // public token='BuRgRcARt-client-Ang';
+  // public token2='ItIsfoRSaFEtY;!@)(@';
+  // constructor(private _http:Http) { }
 
-  headerCTJsontoken(token,token2) {
-    let header = new Headers({ 'content-type': 'application/json' });
-    header.append('Access-Control-Allow-Origin', '*')
-    header.append("Client_Service", token);
-    header.append("Auth_Key", token2);
-    return header;
-  }
+  // headerCTJsontoken(token,token2) {
+  //   let header = new Headers({ 'content-type': 'application/json' });
+  //   header.append('Access-Control-Allow-Origin', '*')
+  //   header.append("Client_Service", token);
+  //   header.append("Auth_Key", token2);
+  //   return header;
+  // }
+  //
+  // getAllProducts() {
+  //   let httpOption = this.headerCTJsontoken(this.token,this.token2);
+  //   const option = new RequestOptions({headers: httpOption});
+  //   // var action = "getAllStudentMarks?classId=" + classId;
+  //   let url = 'http://hopenotout.pk/burger/index.php/Book/getProducts'
+  //
+  //   // console.log(url);
+  //   return this._http.get(url, option).map(
+  //       response => response.json()
+  //   );
 
-  getAllProducts() {
-    let httpOption = this.headerCTJsontoken(this.token,this.token2);
-    const option = new RequestOptions({headers: httpOption});
-    // var action = "getAllStudentMarks?classId=" + classId;
-    let url = 'http://hopenotout.pk/burger/index.php/Book/getProducts'
-
-    // console.log(url);
-    return this._http.get(url, option).map(
-        response => response.json()
-    );
-
-  }
+  // }
 
   // public ip = 'https://www.cricdreamteam.com';//'http://api.coraborate.com:82'
   // public port = '';
